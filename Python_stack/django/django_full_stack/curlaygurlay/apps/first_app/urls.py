568831a6b02bc73d4c0837e51b3814f58501e71a
Lindsey Vaughn <lindsey.l.vaughn@gmail.com>
2019-10-29 09:30:42 -0700

    new projects

--- Python_stack/django/django_full_stack/curlaygurlay/apps/first_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-    url(r'^curlaygurlay$', views.home_page),
-    url(r'^curlaygurlay/loose_curl$', views.loose_curl),
-    url(r'^curlaygurlay/tight_curl$', views.tight_curl),
-    url(r'^curlaygurlay/kinky_curl$', views.kinky_curl),
-]
